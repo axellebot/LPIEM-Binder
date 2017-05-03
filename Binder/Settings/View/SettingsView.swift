94baf3a6021f0b541f4b9deaded910a666451579
Axel Turlier <axel.turlier@outlook.fr>
2017-05-03 17:15:04 +0200

    Modified the Payment view. Now using Stripe's default one

--- Binder/Settings/View/SettingsView.swift
+++ Binder/Settings/View/SettingsView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Stripe
 
 class SettingsView: UIViewController {
 
@@ -16,7 +17,7 @@ class SettingsView: UIViewController {
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        self.performSegue(withIdentifier: "settingsToCheckout", sender: self)
+        displayStripeAddCardController()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,6 +25,28 @@ class SettingsView: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func displayStripeAddCardController() {
+        let addCardViewController = STPAddCardViewController()
+        addCardViewController.delegate = self
+        // STPAddCardViewController must be shown inside a UINavigationController.
+        let navigationController = UINavigationController(rootViewController: addCardViewController)
+        STPTheme.default().accentColor = UIColor(red:0.25, green:0.32, blue:0.71, alpha:1.00)
+        self.present(navigationController, animated: true, completion: nil)
+    }
+    
+    func chargeUser(token: STPToken, amount: Float) {
+        
+    }
+    
+}
 
+extension SettingsView : STPAddCardViewControllerDelegate {
+    func addCardViewControllerDidCancel(_ addCardViewController: STPAddCardViewController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func addCardViewController(_ addCardViewController: STPAddCardViewController, didCreateToken token: STPToken, completion: @escaping STPErrorBlock) {
+        chargeUser(token: token, amount: 3.99);
+    }
 }
 
